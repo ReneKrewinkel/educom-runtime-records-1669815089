028598c9ab2aa8d14288a9e20d3cf3e38346a0a3
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-12-05 13:15:14 +0100

    label atom (#10)

--- src/components/atoms/Label/Label.js
+++ src/components/atoms/Label/Label.js
@@ -1,38 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-const Label = ({ text,
-                 exceptionType = "",
-                 additionalClasses, testID}) => {
-  
-  const utils = ["Label", ...additionalClasses || []].join(" ")
-  const exType = exceptionType ?? ""
+const Label = ({ text, exceptionType = "", additionalClasses, testID }) => {
+  const utils = ["Label", ...(additionalClasses || [])].join(" ");
+  const exType = exceptionType ?? "";
 
-  return(
-        <span data-testid={ testID } data-object-type={exType}
-              className={ utils }>
-            { text }
-        </span>
-  )
-  
-}
-
+  return (
+    <span data-testid={testID} data-object-type={exType} className={utils}>
+      {text}
+    </span>
+  );
+};
 
 const utilityClasses = [
-    "red",
-    "blue"
-]
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "fg-highlight",
+  "fg-highlight-light-50",
+  "fg-highlight-dark-50",
+];
 
-const exceptionClasses = [
-    "button",
-    "regular"
-]
+const exceptionClasses = ["button", "regular"];
 
 Label.propTypes = {
-    text: PropTypes.string.isRequired,
-    exceptionType: PropTypes.oneOf(exceptionClasses),
-    testID: PropTypes.string,
-    additionalClasses: PropTypes.arrayOf(PropTypes.oneOf(utilityClasses)),
-}
+  text: PropTypes.string.isRequired,
+  exceptionType: PropTypes.oneOf(exceptionClasses),
+  testID: PropTypes.string,
+  additionalClasses: PropTypes.arrayOf(PropTypes.oneOf(utilityClasses)),
+};
 
-export default Label
+export default Label;
--- src/components/atoms/Label/Label.stories.js
+++ src/components/atoms/Label/Label.stories.js
@@ -1,23 +1,65 @@
-import React from 'react'
-import Label from './Label'
+import React from "react";
+import Label from "./Label";
 
 const LabelMeta = {
-    title: "atoms/Label",
-    component: Label, 
-    argTypes: {
-        testID: { table: { disable: true } },
-        additionalClasses: { name: "Utility Classes" }
-    }   
-}
+  title: "atoms/Label",
+  component: Label,
+  argTypes: {
+    testID: { table: { disable: true } },
+    additionalClasses: { name: "Utility Classes" },
+  },
+};
 
-const Template = (args) => <Label {...args} />
+const Template = (args) => <Label {...args} />;
 
-const DefaultText = "Default Label"
+const DefaultText = "Default Label";
 
-export const DefaultLabel = Template.bind({})
-DefaultLabel.args = {
-    text: DefaultText,
-    additionalClasses: []
-}
+export const SmallOrangeLabel = Template.bind({});
+SmallOrangeLabel.args = {
+  text: "indie, math rock, noise rock",
+  additionalClasses: ["fg-highlight", "main-text-bold-12"],
+};
 
-export default LabelMeta
+export const GreyLabel = Template.bind({});
+GreyLabel.args = {
+  text: "Mythic Sunship",
+  additionalClasses: ["fg-base-light-50", "h5"],
+};
+
+export const FooterItemLabel = Template.bind({});
+FooterItemLabel.args = {
+  text: "info@runtime-records.com",
+  additionalClasses: ["fg-base-light-50", "main-text-bold-12"],
+};
+export const HeroHeaderLabel = Template.bind({});
+HeroHeaderLabel.args = {
+  text: "Book of Curse",
+  additionalClasses: ["fg-highlight-light-50", "h2"],
+};
+export const FooterHeaderLabel = Template.bind({});
+FooterHeaderLabel.args = {
+  text: "Info",
+  additionalClasses: ["fg-highlight-light-50", "h3"],
+};
+export const H4Label = Template.bind({});
+H4Label.args = {
+  text: "Spiderland",
+  additionalClasses: ["fg-highlight-dark-50", "h4"],
+};
+export const H1Label = Template.bind({});
+H1Label.args = {
+  text: "shopping cart",
+  additionalClasses: ["fg-highlight-dark-50", "h1"],
+};
+export const H3Label = Template.bind({});
+H3Label.args = {
+  text: "Another Shape of Psychedelic Music",
+  additionalClasses: ["fg-highlight-dark-50", "h3"],
+};
+export const H2Label = Template.bind({});
+H2Label.args = {
+  text: "total order",
+  additionalClasses: ["fg-highlight-dark-50", "h2"],
+};
+
+export default LabelMeta;
