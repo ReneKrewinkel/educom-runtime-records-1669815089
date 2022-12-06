ba3cc818c966dd671d4e30d45fc3ccf62279e5b6
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-12-06 11:06:11 +0100

    added 2nd logo (#10)

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -1,44 +1,43 @@
-import PropTypes from 'prop-types'
-import logoBlack from '../../../resources/images/logo-black.svg'
-import logoWhite from '../../../resources/images/logo-white.svg'
+import PropTypes from "prop-types";
+import logoBlack from "../../../resources/images/logo-black.svg";
+import logoWhite from "../../../resources/images/logo-white.svg";
+import logo2 from "../../../resources/images/record-store-day.svg";
 
 /// Default Props defined in Logo.defaultProps
 const Logo = (props) => {
+  // const size = props.size ? props.size : "small"
+  const logos = [
+    { type: "logo-black", element: logoBlack },
+    { type: "logo-white", element: logoWhite },
+    { type: "logo2", element: logo2 },
+  ];
+  const logo = logos.find((type) => type.type == props.exceptionType).element;
 
-    const logo = props.exceptionType === "logo-white" ? logoWhite : logoBlack
-   // const size = props.size ? props.size : "small"
+  return (
+    <div
+      data-testid={props.testID}
+      data-object-type={props.exceptionType ?? ""}
+      className={"Logo"}
+    >
+      <img src={logo} className={props.size} alt="Logo" />
+    </div>
+  );
+};
 
-    return(
-        <div data-testid={ props.testID } 
-             data-object-type={ props.exceptionType ?? ""}
-             className={ "Logo" }>
-            <img src={ logo } className={ props.size } alt="Logo" />
-        </div>
-  )
-  
-}
+const sizes = ["small", "medium", "large"];
 
-const sizes = [
-    "small",
-    "medium",
-    "large"
-]
-
-const exceptionClasses = [
-    "logo-black",
-    "logo-white"
-]
+const exceptionClasses = ["logo-black", "logo-white"];
 
 Logo.propTypes = {
-    testID: PropTypes.string,
-    exceptionType: PropTypes.oneOf(exceptionClasses),
-    size: PropTypes.oneOf(sizes),
-}
+  testID: PropTypes.string,
+  exceptionType: PropTypes.oneOf(exceptionClasses),
+  size: PropTypes.oneOf(sizes),
+};
 
 Logo.defaultProps = {
-    testID: `Logo-${ Math.floor(Date.now() / 1000) }`,
-    exceptionType: 'logo-black',
-    size: 'medium'
-}
+  testID: `Logo-${Math.floor(Date.now() / 1000)}`,
+  exceptionType: "logo-black",
+  size: "medium",
+};
 
-export default Logo
+export default Logo;
--- src/components/atoms/Logo/Logo.stories.js
+++ src/components/atoms/Logo/Logo.stories.js
@@ -1,38 +1,44 @@
-import React from 'react'
-import Logo from './Logo'
+import React from "react";
+import Logo from "./Logo";
 
 const LogoMeta = {
-    title: "Atoms/Logo",
-    component: Logo, 
-    argTypes: {
-        testID: { table: { disable: true } },
-        size: { name: "Size"},
-        exceptionType: { name: "Exception Type"}
-    }   
-}
+  title: "Atoms/Logo",
+  component: Logo,
+  argTypes: {
+    testID: { table: { disable: true } },
+    size: { name: "Size" },
+    exceptionType: { name: "Exception Type" },
+  },
+};
 
-const testID = "" + Math.floor(Math.random() * 90000) + 10000
-const Template = (args) => <Logo {...args} />
+const testID = "" + Math.floor(Math.random() * 90000) + 10000;
+const Template = (args) => <Logo {...args} />;
 
-export const DefaultLogo = Template.bind({})
-DefaultLogo.args = Logo.defaultProps
+export const DefaultLogo = Template.bind({});
+DefaultLogo.args = Logo.defaultProps;
 
-export const SmallLogo = Template.bind({})
+export const SmallLogo = Template.bind({});
 SmallLogo.args = {
-    size: 'small',
-    exceptionType: 'logo-black'
-}
+  size: "small",
+  exceptionType: "logo-black",
+};
 
-export const MediumLogo = Template.bind({})
+export const MediumLogo = Template.bind({});
 MediumLogo.args = {
-    size: 'medium',
-    exceptionType: 'logo-black'
-}
+  size: "medium",
+  exceptionType: "logo-black",
+};
 
-export const LargeLogo = Template.bind({})
+export const LargeLogo = Template.bind({});
 LargeLogo.args = {
-    size: 'large',
-    exceptionType: 'logo-black'
-}
+  size: "large",
+  exceptionType: "logo-black",
+};
 
-export default LogoMeta
+export const SecondLogo = Template.bind({});
+SecondLogo.args = {
+  size: "extra-large",
+  exceptionType: "logo2",
+};
+
+export default LogoMeta;
